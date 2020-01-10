389766287efcfd293218c5af3c1ec5fbc71919ed
royweb <xytank@gmail.com>
2020-01-10 14:04:01 -0500

    Set up mouse.js and buttonState.js

---
+++ input/mouse.js
@@ -0,0 +1,11 @@
+function MouseHandler(){
+    this.left = new ButtonState();
+    this.middle = new ButtonState();
+    this.right = new ButtonState();
+
+    this.position = new Vector2();
+
+    document.onmousemove = handleMouseMove;
+    document.onmousedown = handleMouseDown;
+    document.onmouseup = handleMouseUp;
+}
