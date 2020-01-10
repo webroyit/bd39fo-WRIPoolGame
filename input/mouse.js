9a82c4bd770af7ef57801001735dfd38965650f3
royweb <xytank@gmail.com>
2020-01-10 14:23:02 -0500

    Add functions for handleMouseMove and handleMouseDown

--- input/mouse.js
+++ input/mouse.js
@@ -1,3 +1,34 @@
+function handleMouseMove(e){
+    let x = e.pageX;
+    let y = y.pageY;
+
+    Mouse.position = new Vertor2(x, y);
+}
+
+function handleMouseDown(e){
+    handleMouseMove(e);
+
+    // which check which button on the mouse is press
+    if(e.which === 1){
+        if(!Mouse.left.down){
+            Mouse.left.pressed = true;
+        }
+        Mouse.left.down = true;
+    }
+    else if(e.which === 2){
+        if(!Mouse.middle.down){
+            Mouse.middle.pressed = true;
+        }
+        Mouse.middle.down = true;
+    }
+    else if(e.which === 3){
+        if(!Mouse.right.down){
+            Mouse.right.pressed = true;
+        }
+        Mouse.right.down = true;
+    }
+}
+
 function MouseHandler(){
     this.left = new ButtonState();
     this.middle = new ButtonState();
@@ -9,3 +40,5 @@ function MouseHandler(){
     document.onmousedown = handleMouseDown;
     document.onmouseup = handleMouseUp;
 }
+
+let Mouse = new MouseHandler();
