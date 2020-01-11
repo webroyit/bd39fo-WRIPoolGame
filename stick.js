1069d074fe7b4b7fe0786abe73079d1a57983924
royweb <xytank@gmail.com>
2020-01-11 13:50:02 -0500

    Add a function to rotate the stick around the white ball based on the mouse movement

--- stick.js
+++ stick.js
@@ -2,17 +2,22 @@ const STICK_ORIGIN = new Vector2(970, 11);
 
 function Stick(position){
     this.position = position;
+    this.rotation = 0;
 }
 
 Stick.prototype.update = function(){
-    // for testing
-    // this.position = Mouse.position;
-
-    // if(Mouse.left.pressed){
-    //     console.log("Pressed left");
-    // }
+    this.updateRotation();
 }
 
 Stick.prototype.draw = function(){
-    Canvas.drawImage(sprites.stick, this.position, STICK_ORIGIN);
+    Canvas.drawImage(sprites.stick, this.position, STICK_ORIGIN, this.rotation);
+}
+
+Stick.prototype.updateRotation = function(){
+    // get the position of the mouse pointer
+    let opposite = Mouse.position.y - this.position.y;
+    let adjacent = Mouse.position.x  -this.position.x;
+
+    // calculate the angle
+    this.rotation = Math.atan2(opposite, adjacent);
 }
