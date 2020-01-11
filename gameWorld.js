d8513b6f8d67dac70816cf9d82bb5720bf981e89
royweb <xytank@gmail.com>
2020-01-11 13:35:53 -0500

    Show the white ball on the screen

--- gameWorld.js
+++ gameWorld.js
@@ -1,15 +1,19 @@
 // update the game
 function GameWorld(){
+    this.whiteBall = new Ball(new Vector2(413, 413));
     this.stick = new Stick();
 }
 
 GameWorld.prototype.update = function(){
     this.stick.update();
+    this.whiteBall.update();
 }
 
+// display images
 GameWorld.prototype.draw = function(){
 
     Canvas.drawImage(sprites.background, {x: 0, y: 0});
 
     this.stick.draw();
+    this.whiteBall.draw();
 }
