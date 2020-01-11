66e607b305841377428920e127dce24ac77d8a6f
royweb <xytank@gmail.com>
2020-01-11 14:14:00 -0500

    Add a function that determine the power of hitting the white ball

--- stick.js
+++ stick.js
@@ -4,9 +4,14 @@ function Stick(position){
     this.position = position;
     this.rotation = 0;
     this.origin = STICK_ORIGIN.copy();
+    this.power = 0;
 }
 
 Stick.prototype.update = function(){
+    if(Mouse.left.down){
+        this.increasePower();
+    }
+
     this.updateRotation();
 }
 
@@ -21,4 +26,9 @@ Stick.prototype.updateRotation = function(){
 
     // calculate the angle
     this.rotation = Math.atan2(opposite, adjacent);
+}
+
+Stick.prototype.increasePower = function(){
+    this.power += 100;      // how hard to hit the ball
+    this.origin.x += 5;     // move the stick away from white ball
 }
