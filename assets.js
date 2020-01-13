37c2a082ce79b1fdd70e340b0dae04d3bb40f824
royweb <xytank@gmail.com>
2020-01-13 13:56:03 -0500

    Add more images of the balls

--- assets.js
+++ assets.js
@@ -32,6 +32,9 @@ function loadAssets(callback){
     sprites.background = loadSprite("spr_background.png");
     sprites.stick = loadSprite("spr_stick.png");
     sprites.whiteBall = loadSprite("spr_whiteBall.png");
+    sprites.redBall = loadSprite("spr_redBall.png");
+    sprites.yellowBall = loadSprite("spr_yellowBall.png");
+    sprites.blackBall = loadSprite("spr_blackBall.png");
 
     assetsLoadingLoop(callback);
 }
